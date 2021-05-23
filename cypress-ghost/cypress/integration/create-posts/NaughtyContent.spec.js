65168bfd524b8b929bafd8ef93424230c3cf3cc2
Juan Camilo Daza <jc.dazam1@uniandes.edu.co>
2021-05-23 01:01:51 -0500

    Pruebas aleatorias apriori

---
+++ cypress-ghost/cypress/integration/create-posts/NaughtyContent.spec.js
@@ -0,0 +1,50 @@
+/// <reference types='cypress' />
+import MenuPage from "../pageObjects/MenuPage";
+import PostsPage from "../pageObjects/PostsPage";
+import PostListPage from "../pageObjects/PostListPage";
+
+const postList = require("../../fixtures/create-posts/Mokaroo-Post-NaughtyContent.json");
+
+postList.forEach((post) => {
+  context("Create draft post with characters differents #" + post.id, () => {
+    before(() => {
+      cy.login();
+    });
+
+    beforeEach(() => {
+      Cypress.Cookies.preserveOnce("ghost-admin-api-session");
+    });
+
+    after(() => {
+      cy.logout();
+    });
+
+    it("should navigate to /posts from home", () => {
+      MenuPage.getPostsLink().click();
+      cy.url().should("include", "ghost/#/posts");
+    });
+
+    it("should navigate to post editor by clicking new page button", () => {
+      PostsPage.getNewPostButton().click();
+      cy.url().should("include", "ghost/#/editor/post");
+    });
+
+    it("should create an untitled draft post when no information is provided", () => {
+      PostsPage.getTitleField().click();
+      PostsPage.getBackToPostsPageButton().click();
+
+      PostListPage.getLastDraftPostTitle().should("contain.text", "(Untitled)");
+    });
+
+    it("should fill inputs and update information page", () => {
+      PostListPage.getLastDraftPostTitle().click({ force: true });
+
+      PostsPage.getTitleField().clear().type(post.title);
+      PostsPage.getContentField().type(post.content);
+
+      PostsPage.getBackToPostsPageButton().click();
+      PostListPage.getLastDraftPostTitle().should("contain.text", post.title);
+      cy.wait(300);
+    });
+  });
+});
